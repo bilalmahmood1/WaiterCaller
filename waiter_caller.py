2e5705521b79e5d73eafc6fe79f013c4a7f6f9f7
Bilal Mahmood <bmahmood328@gmail.com>
2018-07-07 03:04:44 +0500

    Added Bootstrap Login page

--- waiter_caller.py
+++ waiter_caller.py
@@ -6,21 +6,16 @@ Created on Thu Jun 28 20:49:48 2018
 @author: bilal
 """
 
-
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def home():
-    return "Site under construction"
-
-
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 5001, debug=True)
     
     
