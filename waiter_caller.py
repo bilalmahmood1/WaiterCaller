6cc45d7ac2b560465d025bdbc6ec13b10a20d9c4
Bilal Mahmood <bmahmood328@gmail.com>
2018-07-18 21:27:54 +0500

    Added WTForms to all the forms used in the app

--- waiter_caller.py
+++ waiter_caller.py
@@ -26,6 +26,12 @@ from helper import HelperLibrary
 from bitlyhelper import BitlyHelper
 import datetime
 
+from forms import RegistrationForm
+from forms import LoginForm
+from forms import CreateTableForm
+from forms import DeleteTableForm
+from forms import ResolveRequestForm
+
 app = Flask(__name__)
 app.secret_key = 'ae+4YjO+l+GMTjsMPu+JNpFb4g55YbGgGSfIBt57mNWbiB/PF7kNGQQ4KCqiCkSrCAmVGMvku00GuJoMkFV3xTaAGA+5/f4Bqye'
 login_manager = LoginManager()
@@ -55,7 +61,11 @@ def unauthorized():
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    registrationform = RegistrationForm()
+    loginform = LoginForm()
+    return render_template("home.html",
+        loginform = loginform,
+        registrationform = registrationform)
 
 
 @app.route("/dashboard")
@@ -63,6 +73,7 @@ def home():
 def dashboard():
     now = datetime.datetime.now()
     requests = DB.get_requests(current_user.get_id())
+    form = ResolveRequestForm()
     for req in requests:
         deltaseconds = (now - req['datetime']).seconds
         req['wait_minutes'] = "{} minutes {} seconds".format((deltaseconds//60), str(deltaseconds % 60).zfill(2))
@@ -70,57 +81,81 @@ def dashboard():
     return render_template("dashboard.html",
     					   user = current_user.get_id(),
                         calls = len(requests),
-                        requests = requests)
+                        requests = requests,
+                        resolverequestform = form)
 
 @app.route("/account")
 @login_required
 def account():
+    form_create_table = CreateTableForm()
+    form_delete_table = DeleteTableForm()
     return render_template("account.html",
                            user = current_user.get_id(),
+                           createtableform = form_create_table,
+                           deletetableform = form_delete_table,
                            table = DB.get_tables(current_user.get_id()))
-
 
 
 @app.route("/login", methods=['POST'])
 def login():
-    email = request.form.get("email")
-    entered_password = request.form.get("password")
-    user_credentials = DB.get_user(email)
     
-    if user_credentials:
-        hashed = user_credentials["hashed"]
-        salt = user_credentials["salt"]
-        if PH.validate_password(entered_password, salt, hashed):
-            user = User(email)
-            login_user(user)        
-            return redirect(url_for('dashboard'))
-        ## Incorrect password
+    loginform = LoginForm(request.form)
+    
+    if loginform.validate_on_submit():
+        user_credentials = DB.get_user(loginform.email.data)
+        if user_credentials:
+             hashed = user_credentials["hashed"]
+             salt = user_credentials["salt"]
+             if PH.validate_password(loginform.password.data, salt, hashed): 
+                user = User(loginform.email.data)
+                login_user(user)        
+                return redirect(url_for('dashboard'))
+             else:
+                 loginform.password.errors.append("Invalid password entered!")
+                 return render_template("home.html",
+                            loginform = loginform,
+                            loginfail = True,
+                            registrationform = RegistrationForm())
+    
         else:
-            return redirect(url_for('home'))
-    ## Please register
+            loginform.email.errors.append("Kindly register!")
+            return render_template("home.html",
+                            loginform = loginform,
+                            loginfail = True,
+                            registrationform = RegistrationForm())
+    
     else:
-        return redirect(url_for('home'))
-
-
+        return render_template("home.html",
+                            loginform = loginform,
+                            loginfail = True,
+                            registrationform = RegistrationForm())
+    
+    
 @app.route("/register", methods=['POST'])
 def register():
-    email = request.form.get("email")
-    password1 = request.form.get("password")
-    password2 = request.form.get("password2")
+    form = RegistrationForm(request.form)
     
-    if DB.get_user(email):
-        return redirect(url_for('home'))
-     
-    if password1 != password2:        
-        return redirect(url_for('home'))
-     
-    salt = PH.get_salt()
-    hashed = PH.get_hash(password1 + salt)
-    DB.add_user(email, salt, hashed)
-     
-    return redirect(url_for('home'))
+    if form.validate_on_submit():        
+        if DB.get_user(form.email.data):
+            form.email.errors.append("Email already registered!")
+            return render_template("home.html",
+                            loginform = LoginForm(),
+                            registrationform = form)
+    
+        salt = PH.get_salt()
+        hashed = PH.get_hash(form.password.data + salt)
+        DB.add_user(form.email.data, salt, hashed)
+        return render_template("home.html",
+                            registrationform = form,
+                            loginform = LoginForm(),
+                            successRegistration = True)
+        
+    return render_template("home.html",
+                            loginform = LoginForm(),
+                            registrationform = form)
 
-     
+    
+    
 @app.route("/logout")
 def logout():
     logout_user()
@@ -130,11 +165,20 @@ def logout():
 @app.route("/account/createtable", methods=["POST"])
 @login_required
 def account_createtable():
-    tablename = request.form.get("tablenumber")
-    tableid = DB.add_table(tablename, current_user.get_id())
-    new_url = config.base_url + "newrequest/" + str(tableid)
-    DB.update_table(tableid, BH.shorten_url(new_url))
-    return redirect(url_for('account'))
+    form = CreateTableForm(request.form)
+    if form.validate_on_submit():
+        tablename = form.tablenumber.data
+        tableid = DB.add_table(tablename, current_user.get_id())
+        new_url = config.base_url + "newrequest/" + str(tableid)
+        DB.update_table(tableid, BH.shorten_url(new_url))
+        return redirect(url_for('account'))
+    else:
+        return render_template("account.html",
+                               user = current_user.get_id(),
+                               createtableform = form,
+                               deletetableform = DeleteTableForm(),
+                               table = DB.get_tables(current_user.get_id()))
+
 
 @app.route("/account/deletetable")
 @login_required
@@ -153,20 +197,6 @@ def dashboard_resolve():
     return redirect(url_for('dashboard'))
 
 
-## Version with the table_id in the URL;
-"""
-@app.route('/newrequest/<tid>')
-def newrequest_tid(tid):
-    table_id = int(tid)
-    table = DB.get_table_id(table_id)
-    table_number = table["number"]
-    table_owner = table["owner"]
-    time = datetime.datetime.now()
-    DB.add_request(table_id, time,table_number,table_owner)
-    return render_template("newrequest.html",
-                           table_number = table_number)
-
-"""
 
 @app.route('/newrequest/<tid>')
 def newrequest_tid(tid):
